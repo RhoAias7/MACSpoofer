bd2ac0b53906b2fcffd752e76018704ae985c1f3
root <ayofouad@gmail.com>
2020-03-24 20:55:21 +0000

    Completed MAC Spoofer

--- MACSpoofer.py
+++ MACSpoofer.py
@@ -2,9 +2,9 @@
 
 import subprocess
 import optparse
+import re
 
-
-def get_arguments():
+def get_args():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change the MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
@@ -15,14 +15,34 @@ def get_arguments():
         parser.error("[-] Please specify a new MAC address, user --help for more info")
     return options
 
+def get_current_mac_address(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', ifconfig_result)
 
-def spoof_mac(interface, new_mac_address):
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+def spoof_mac_address(interface, new_mac_address):
     print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
 
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
     subprocess.call(["ifconfig", interface, "up"])
 
+options = get_args()
 
-options = get_arguments()
-spoof_mac(options.interface, options.new_mac)
+current_mac_address = get_current_mac_address(options.interface)
+print("Current MAC = " + str(current_mac_address))
+
+spoof_mac_address(options.interface, options.new_mac)
+
+current_mac_address = get_current_mac_address(options.interface)
+
+if current_mac_address == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac_address)
+else:
+    print("[-] MAC address did not get changed.")
+
+
