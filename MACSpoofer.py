44dba2d614f3e09b7ae09ece6697ddbaf6cfbd93
root <ayofouad@gmail.com>
2020-03-24 20:06:58 +0000

    MAC spoofer now takes cmd arguments from user

--- MACSpoofer.py
+++ MACSpoofer.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
-import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+import subprocess
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change the MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, user --help for more info")
+    if not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, user --help for more info")
+    return options
+
+
+def spoof_mac(interface, new_mac_address):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac_address)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+spoof_mac(options.interface, options.new_mac)
